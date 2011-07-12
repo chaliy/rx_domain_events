65d628e12ad574c6b050f8b03306fb70677a7e3d
Mike Chaliy <mike@chaliy.name>
2011-07-12 09:35:24 +0300

    Started working under denormalized views

---
+++ src/TryComplexRx/Domain/AccountListDenormalizer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TryComplexRx.Domain
+{
+    public class AccountListDenormalizer : IObserver<object>
+    {
+        public class AccountListItem
+        {
+            public string Number { get; set; }
+            public decimal CurrentBalance { get; set; }
+            public DateTime LastUpdate { get; set; }
+            public string Context { get; set; }
+        }
+        
+        public void OnNext(object value)
+        {
+
+        }
+
+        public void OnError(Exception error)
+        {            
+        }
+
+        public void OnCompleted()
+        {
+        }
+    }
+}
